d545e5ba998045556a5d8d546647be767d5ff9be
iqran-s-star <iqrankhan163@gmail.com>
2022-09-27 02:46:36 +0500

    basic setup scheduled tweets

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,11 @@
+class HomeController < ApplicationController
+  def index
+    # flash[:notice] =" You are on the home page"
+    # flash[:alert] ="This is a home page alert"
+  end
+
+  def about
+    # flash[:notice] =" You are navigate to the about  page"
+    # flash[:alert] ="This is a about page alert"
+  end
+end
